4281e678bdff233ed9b0ee81daa7a0f931bbb340
AliaksandrLiashevich <43993109+AliaksandrLiashevich@users.noreply.github.com>
2018-11-26 11:10:43 +0300

    Task 2

---
+++ No2.Solution/EventHandlers.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace No2
+{
+    public static class EventHandlers
+    {
+        public static void BankSells(object sender, BankStateEventArgs e)
+        {
+            Console.WriteLine(e.Type + ":" + e.Money);
+        }
+
+        public static void BankBuying(object sender, BankStateEventArgs e)
+        {
+            Console.WriteLine(e.Type + ":" + e.Money);
+        }
+
+        public static void BrokerSells(object sender, BankStateEventArgs e)
+        {
+            Console.WriteLine(e.Type + ":" + e.Money);
+        }
+
+        public static void BrokerBuying(object sender, BankStateEventArgs e)
+        {
+            Console.WriteLine(e.Type + ":" + e.Money);
+        }
+
+    }
+}
---
+++ No2/BankStateEventArgs.cs
@@ -0,0 +1,15 @@
+﻿namespace No2
+{
+    public class BankStateEventArgs
+    {
+        public BankStateEventArgs(int _money, string _type)
+        {
+            Money = _money;
+
+            Type = _type;
+        }
+
+        public int Money { get; private set; }
+        public string Type { get; private set; }
+    }
+}
