e6668dcf83f7f858fdf0170e6f37b4458efe6b57
AliaksandrLiashevich <43993109+AliaksandrLiashevich@users.noreply.github.com>
2018-11-26 10:36:28 +0300

    Task1
    Delegates List<>, that contains delegates and registred methods for them.

---
+++ No1.Solution.Tests/PasswordCheckerServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace No1.Solution.Tests
+{
+    [TestFixture]
+    public class PasswordCheckerServiceTests
+    {
+        static private List<ValidationDelegate> list1 = new List<ValidationDelegate>() { ValidationMethods.EmptyPassword,
+        ValidationMethods.IsContainsChar, ValidationMethods.LengthLessThanSeven};
+
+        static private List<ValidationDelegate> list2 = new List<ValidationDelegate>() { ValidationMethods.EmptyPassword,
+        ValidationMethods.IsContainsChar, ValidationMethods.LengthLessThanSeven, ValidationMethods.IsContainsUpperChar};
+
+        static private List<ValidationDelegate> list3 = new List<ValidationDelegate>() { ValidationMethods.EmptyPassword,
+        ValidationMethods.IsContainsChar, ValidationMethods.IsContainsDigits, ValidationMethods.LengthLessThanSeven,
+        ValidationMethods.IsContainsUpperChar};
+
+        [TestCase("1234", false)]
+        [TestCase("123Jack00", true)]
+
+        public void AddPasword_List1_BoolResult(string password, bool result)
+        {
+            PasswordCheckerService service = new PasswordCheckerService(list1);
+
+            Assert.AreEqual(result, service.AddPasword(password).Item1);
+        }
+
+        [TestCase("1234", false)]
+        [TestCase("123lack00", false)]
+        [TestCase("123Jack00", true)]
+
+        public void AddPasword_List2_BoolResult(string password, bool result)
+        {
+            PasswordCheckerService service = new PasswordCheckerService(list2);
+
+            Assert.AreEqual(result, service.AddPasword(password).Item1);
+        }
+
+        [TestCase("1234", false)]
+        [TestCase("GoodLuck", false)]
+        [TestCase("Learning120", true)]
+
+        public void AddPasword_List3_BoolResult(string password, bool result)
+        {
+            PasswordCheckerService service = new PasswordCheckerService(list3);
+
+            Assert.AreEqual(result, service.AddPasword(password).Item1);
+        }
+    }
+}
---
+++ No1.Solution/ValidationMethods.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Linq;
+
+namespace No1
+{
+    public static class ValidationMethods
+    {
+        public static Tuple<bool, string> EmptyPassword(string password)
+        {
+            if (password == string.Empty)
+                return Tuple.Create(false, $"{password} is empty ");
+
+            return Tuple.Create(true, $"{password} is not empty");
+        }
+
+        public static Tuple<bool, string> LengthLessThanSeven(string password)
+        {
+            // check if length more than 7 chars 
+            if (password.Length <= 7)
+                return Tuple.Create(false, $"{password} length too short");
+
+            return Tuple.Create(true, $"{password} length is more than 7");
+        }
+
+        public static Tuple<bool, string> LengthMoreThanFifteen(string password)
+        {
+            // check if length more than 10 chars for admins
+            if (password.Length >= 15)
+                return Tuple.Create(false, $"{password} length too long");
+
+            return Tuple.Create(true, $"{password} length is less than 15");
+        }
+
+        public static Tuple<bool, string> IsContainsChars(string password)
+        {
+            /// check if password contains at least one alphabetical character 
+            if (!password.Any(char.IsLetter))
+                return Tuple.Create(false, $"{password} hasn't alphanumerical chars");
+
+            return Tuple.Create(true, $"{password} has alphanumerical chars");
+        }
+
+        public static Tuple<bool, string> IsContainsDigits(string password)
+        {
+            // check if password contains at least one digit character 
+            if (!password.Any(char.IsNumber))
+                return Tuple.Create(false, $"{password} hasn't digits");
+
+            return Tuple.Create(true, $"{password} contains digits");
+        }
+
+        public static Tuple<bool, string> IsContainsUpperChar(string password)
+        {
+            /// check if password contains at least one uper symbol 
+            if (!password.Any(char.IsUpper))
+                return Tuple.Create(false, $"{password} hasn't upper chars");
+
+            return Tuple.Create(true, $"{password} has upper chars");
+        }
+
+        public static Tuple<bool, string> IsContainsChar(string password)
+        {
+            /// check if password contains at least one lower symbol
+            if (!password.Any(char.IsLower))
+                return Tuple.Create(false, $"{password} hasn't lower chars");
+
+            return Tuple.Create(true, $"{password} has lower chars");
+        }
+    }
+}
